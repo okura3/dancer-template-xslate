55ff37fb9cd3f9504328b19623a56381210297a1
Takeshi OKURA <okura3@gmail.com>
2011-07-27 10:19:05 +0900

    Call Text::Xslate::render by relative template path.

--- lib/Dancer/Template/Xslate.pm
+++ lib/Dancer/Template/Xslate.pm
@@ -1,11 +1,17 @@
 package Dancer::Template::Xslate;
 
+BEGIN {
+  $Dancer::Template::Xslate::VERSION = '0.01';
+}
+
 # ABSTRACT: Text::Xslate wrapper for Dancer
 
 use strict;
 use warnings;
 
 use Text::Xslate;
+use Dancer::Config 'setting';
+use Dancer::FileUtils 'path';
 
 use base 'Dancer::Template::Abstract';
 
@@ -19,9 +25,36 @@ sub init {
     my %args = (
         %{$self->config},
     );
+    my $views = setting('views') || '.';
+    my $path = [ $views ];
 
-    $_engine = Text::Xslate->new(%args);
+    $_engine = Text::Xslate->new(%args, path => $path, );
 }
+
+sub _template_name {
+    my ( $self, $view ) = @_;
+    my $def_tmpl_ext = $self->config->{suffix} || $self->default_tmpl_ext();
+    $def_tmpl_ext =~ s/^\.//;
+    $view .= ".$def_tmpl_ext" if $view !~ /\.\Q$def_tmpl_ext\E$/;
+    return $view;
+}
+
+sub view {
+    my ($self, $view) = @_;
+    $view = $self->_template_name($view);
+    return $view;
+}
+
+sub layout {
+    my ($self, $layout, $tokens, $content) = @_;
+    my $layout_name = $self->_template_name($layout);
+    my $layout_path = path('layouts', $layout_name);
+    my $full_content =
+      Dancer::Template->engine->render($layout_path,
+        {%$tokens, content => $content});
+    $full_content;
+}
+
 
 sub render {
     my ($self, $template, $tokens) = @_;
@@ -57,12 +90,14 @@ You can configure L<Text::Xslate> :
     template: xslate
     engines:
       xslate:
-        cache_dir  => "/www/../xslate_cache",
-        cache      => 1,
-        module =>
+        syntax: TTerse
+        suffix: ".tt"
+        module:
           - Text::Xslate::Bridge::TT2 # to keep partial compatibility
-
+        cache: 1
+        cache_dir: "xslate_cache"
 
 =head1 SEE ALSO
 
 L<Dancer>, L<Text::Xslate>, L<http://xslate.org/>
+
